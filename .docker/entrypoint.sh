6bfbd7d329a3ac47ee6ae0749e11e8a9c003008d
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2024-06-16 22:20:48 +0200

    update Docker entrypoint

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -5,20 +5,13 @@ function usage {
     echo "usage: entrypoint.sh [--process|--help]"
 }
 
-declare -a StringArray=(
-  "co_conc"    "dust"       "nh3_conc"   "nmvoc_conc" "no2_conc"   "no_conc"    "o3_conc"    "pans_conc"
-  "pm10_conc"  "pm2p5_conc" "pmwf_conc"  "sia_conc"   "so2_conc"
-)
-
-function displayData {
-  for val in ${StringArray[@]}; do
-    su -c "Rscript /r/displayData.R $val" $USER
-  done
+function plotSticks {
+  su -c "Rscript /r/plotSticks.R" $USER
 }
 
 if [ "$1" != "" ]; then
     case $1 in
-        -p | --process )  displayData
+        -p | --process )  plotSticks
                           exit
                           ;;
         -h | --help )     usage
